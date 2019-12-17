9d893b77e32a24da321150897b992b62cff7594f
Greene <jungle1900@gmail.com>
2019-12-17 18:56:36 +0800

    first commit

--- README.md
+++ README.md
@@ -1,2 +1,34 @@
 # solarlunar-go
+
 Chinese solar lunar calendar converter
+
+# Installation
+
+```sh
+go get github.com/jetfueltw/solarlunar
+```
+
+# Useage
+
+公曆轉農曆
+```go
+solar := solarlunar.Solar{Year: 2015, Month: 1, Day: 15}
+lunar := solarlunar.SolarToLunar(solar)
+// Lunar {
+//     Year: 2014,
+//     Month: 11,
+//     Day: 25,
+//     IsLeap: false
+// }
+```
+
+農曆轉公曆
+```go
+lunar := solarlunar.Lunar{Year: 2014, Month: 11, Day: 25, IsLeap: false}
+solar := solarlunar.LunarToSolar(lunar)
+// Solar {
+//     Year: 2015,
+//     Month: 1,
+//     Day: 15
+// }
+```
---
+++ solarlunar_test.go
@@ -0,0 +1,131 @@
+package solarlunar_test
+
+import (
+	"io/ioutil"
+	"net/http"
+	"strconv"
+	"strings"
+	"testing"
+	"time"
+
+	"github.com/jetfueltw/solarlunar"
+	"github.com/stretchr/testify/assert"
+)
+
+func TestSolarToLunar(t *testing.T) {
+	assert := assert.New(t)
+	startTime := time.Date(1900, 1, 1, 0, 0, 0, 0, time.UTC)
+	endTime := time.Date(2100, 1, 1, 0, 0, 0, 0, time.UTC)
+
+	for startTime.Before(endTime) {
+		solar := solarlunar.Solar{
+			Year:  startTime.Year(),
+			Month: int(startTime.Month()),
+			Day:   startTime.Day(),
+		}
+
+		lunar := solarlunar.SolarToLunar(solar)
+		expectLunar, err := getSolarToLunar(solar)
+		if !assert.NoError(err) {
+			return
+		}
+		assert.Equal(expectLunar, lunar)
+
+		solar = solarlunar.LunarToSolar(lunar)
+		expectSolar, err := getLunarToSolar(lunar)
+		if !assert.NoError(err) {
+			return
+		}
+		assert.Equal(expectSolar, solar)
+
+		startTime = startTime.AddDate(0, 0, 1)
+	}
+}
+
+/**
+ * https://github.com/isee15/Lunar-Solar-Calendar-Converter
+ * node check.js in javascript folder
+ */
+func getSolarToLunar(solar solarlunar.Solar) (solarlunar.Lunar, error) {
+	resp, err := http.Get("http://localhost:1337/?src=" + strconv.Itoa(solar.Year) + "," + strconv.Itoa(solar.Month) + "," + strconv.Itoa(solar.Day))
+	if err != nil {
+		return solarlunar.Lunar{}, err
+	}
+	defer resp.Body.Close()
+
+	body, err := ioutil.ReadAll(resp.Body)
+	if err != nil {
+		return solarlunar.Lunar{}, err
+	}
+
+	res := strings.Split(string(body), ",")
+
+	year, err := strconv.Atoi(res[0])
+	if err != nil {
+		return solarlunar.Lunar{}, err
+	}
+	month, err := strconv.Atoi(res[1])
+	if err != nil {
+		return solarlunar.Lunar{}, err
+	}
+	day, err := strconv.Atoi(res[2])
+	if err != nil {
+		return solarlunar.Lunar{}, err
+	}
+	isLeap, err := strconv.Atoi(res[3])
+	if err != nil {
+		return solarlunar.Lunar{}, err
+	}
+
+	return solarlunar.Lunar{
+		Year:   year,
+		Month:  month,
+		Day:    day,
+		IsLeap: isLeap == 1,
+	}, nil
+}
+
+/**
+ * https://github.com/isee15/Lunar-Solar-Calendar-Converter
+ * node check.js in javascript folder
+ */
+func getLunarToSolar(lunar solarlunar.Lunar) (solarlunar.Solar, error) {
+	resp, err := http.Get("http://localhost:1337/?src=" + strconv.Itoa(lunar.Year) + "," + strconv.Itoa(lunar.Month) + "," + strconv.Itoa(lunar.Day) + "," + strconv.Itoa(boolToInt(lunar.IsLeap)))
+	if err != nil {
+		return solarlunar.Solar{}, err
+	}
+	defer resp.Body.Close()
+
+	body, err := ioutil.ReadAll(resp.Body)
+	if err != nil {
+		return solarlunar.Solar{}, err
+	}
+
+	res := strings.Split(string(body), ",")
+
+	year, err := strconv.Atoi(res[0])
+	if err != nil {
+		return solarlunar.Solar{}, err
+	}
+	month, err := strconv.Atoi(res[1])
+	if err != nil {
+		return solarlunar.Solar{}, err
+	}
+	day, err := strconv.Atoi(res[2])
+	if err != nil {
+		return solarlunar.Solar{}, err
+	}
+
+	return solarlunar.Solar{
+		Year:  year,
+		Month: month,
+		Day:   day,
+	}, nil
+}
+
+func boolToInt(b bool) int {
+	if b {
+		return 1
+	}
+	return 0
+}
